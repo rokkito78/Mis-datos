3facce674f7f5f151167dd7c9be7dda0db66c68a
rokkito78 <73363944+rokkito78@users.noreply.github.com>
2021-01-16 19:40:44 -0300

    trabajo java mis datos

---
+++ MisDatos.java
@@ -0,0 +1,12 @@
+
+public class MisDatos {
+
+	public static void main(String[] args) {
+	System.out.println("Mi Nombre es Ricardo Almonacid");
+	System.out.println("Tengo 42 años de edad");
+	System.out.println("soy de la ciudad de Temuco");
+	
+
+	}
+
+}
